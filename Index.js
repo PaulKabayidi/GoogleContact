33807906ee40472fe6526e70dea46d8fba203ec2
Paul kabayidi <paulkabayidi2004@gmail.com>
2024-05-20 05:51:44 +0100

    Conclusion

--- Index.js
+++ Index.js
@@ -1,15 +1,14 @@
-window.addEventListener('load' , function()
-
-{
+window.addEventListener('load' , function(){   
     const form = document.getElementById('contact-form');
-    form.addEventListener('submit' , function(e)
-    {
+    form.addEventListener('submit' , function(e){
         e.preventDefault();
 
         alert('formulaire soumis');
         form.requestFullscreen();
-    }
-)
-}
-
-);
+    });
+    
+    const btn_creer_contact = this.document.getElementById('Ccontact');
+    btn_creer_contact.addEventListener('click' , function(e){
+          form.hidden = false;
+    });
+});
